10ad62783c7750ca36d6ee9ae79188279afb0e2d
coderhuang <wyu11080716@gmail.com>
2020-08-27 16:26:43 +0800

    web init

---
+++ chat-app-web/src/views/Login.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="about">
+    <h1>This is an login page</h1>
+    <div>
+      <el-container>
+        <el-aside width="100px">
+
+
+          <el-card class="box-card">
+            <div slot="header" class="clearfix">
+              <span>卡片名称</span>
+              <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <div v-for="o in 4" :key="o" class="text item">
+              {{'列表内容 ' + o }}
+            </div>
+          </el-card>
+
+
+          <el-row class="demo-avatar demo-basic">
+            <el-col :span="12">
+              <div class="demo-basic--circle">
+                <div class="block"><el-avatar :size="50" :src="circleUrl"></el-avatar></div>
+              </div>
+            </el-col>
+          </el-row>
+
+
+        </el-aside>
+        <el-main>
+          <el-input
+                  type="textarea"
+                  :rows="2"
+                  placeholder="请输入内容"
+                  v-model="textarea">
+          </el-input>
+        </el-main>
+      </el-container>
+    </div>
+  </div>
+
+</template>
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        textarea: '',
+        circleUrl: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png",
+        squareUrl: "https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png",
+        sizeList: ["large", "medium", "small"]
+      }
+    }
+  }
+</script>
